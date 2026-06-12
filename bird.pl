140bed84d32c2b7df63b21caa90689f7c8899b65
harign1402sse-sketch <harign1402.sse@saveetha.com>
2026-06-12 09:22:20 +0530

    Add files via upload

---
+++ bird.pl
@@ -0,0 +1,11 @@
+% Facts
+bird(ostrich, cannot_fly).
+bird(penguin, cannot_fly).
+bird(kiwi, cannot_fly).
+bird(eagle, can_fly).
+bird(pigeon, can_fly).
+bird(sparrow, can_fly).
+
+% Rule
+can_fly(Bird) :-
+    bird(Bird, can_fly).
